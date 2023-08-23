538b14083bb74c9f642a15abe14f6ef5b302989b
Eli Summers <40410999+NaNe3@users.noreply.github.com>
2023-08-23 13:36:59 -0600

    Add files via upload

---
+++ scribble/5/index.php
@@ -0,0 +1,11 @@
+<title>UWU</title>
+<link rel="icon" type="image/png" href="../../res/ast/fav.ico">
+<link href="../../css/style.css" rel="stylesheet" type="text/css">
+<link href="../../css/nav.css" rel="stylesheet" type="text/css">
+<link href="../../css/scribble.css" rel="stylesheet" type="text/css">
+<script src="../../js/ajax.min.js"></script>
+
+<script> 
+    $(function(){ $("#con").load("../temp.php"); document.getElementById("con").style.display = "block";});
+</script>
+<div id="con" style="display: none;"></div>
